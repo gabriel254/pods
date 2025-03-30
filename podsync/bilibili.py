6a561f6a4543d963758698be9c006e1881d449e8
gabriel254 <12846878+gabriel254@users.noreply.github.com>
2025-03-30 06:59:55 +0000

    feat: init

---
+++ podsync/bilibili.py
@@ -0,0 +1,150 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import argparse
+import asyncio
+import os
+import re
+import sys
+from pathlib import Path
+
+import dateparser
+import feedparser
+from base import PodSync
+from loguru import logger
+from videogram.utils import delete_files, load_json
+from videogram.ytdlp import ytdlp_extract_info
+from yt_dlp.utils import DownloadError, ExtractorError
+
+
+class Bilibili(PodSync):
+    def __init__(self, name: str, config: dict, database_path: Path) -> None:
+        super().__init__(name, config, database_path)
+
+    def check_entry(self, entry: dict) -> dict:
+        """Check if the entry is valid for download.
+
+        Some videos may not be valid for download, such as upcoming videos, living videos, banned videos, etc.
+        This method checks if the entry is valid for download.
+
+        It should return three keys:
+            - need_update_database: bool, whether the database should be updated.
+            - metadata: dict, metadata of the entry.
+            - need_download: bool, whether the entry should be downloaded.
+
+        For example:
+            - For a banned video, the need_update_database should be true because we need to treat it as processed.
+              But the need_download should be false because we don't need to download it.
+            - For an upcoming video or living video, the need_update_database should be false because we should wait for it to be finished.
+
+        Args:
+            entry (dict): A single entry information from the feedparser.
+
+        Returns:
+            dict: A dictionary contains the information of the entry.
+        """
+        res = {
+            "need_update_database": False,
+            "metadata": {},
+            "need_download": False,
+        }
+        # log metadata
+        publish_time = dateparser.parse(entry["published"], settings={"TO_TIMEZONE": os.getenv("TZ", "UTC")})
+        res["metadata"] = {"title": entry["title"], "vid": Path(entry["link"]).stem, "time": f"{publish_time:%a, %d %b %Y %H:%M:%S %z}"}
+        res["need_update_database"] = True
+        try:
+            # test if the video is available
+            ytdlp_extract_info(entry["link"], use_cookie=False, playlist=False, process=False)[0]
+        except ExtractorError as e:
+            logger.error(f"ExtractorError: {e.msg}")
+            raise
+        except DownloadError as e:
+            logger.error(f"DownloadError: {e.msg}")
+            if "HTTPError 404" in str(e.msg):
+                logger.warning(f"Skip 404 not found video: {entry['title']}")
+                return res
+            if "deleted or geo-restricted" in str(e.msg):
+                logger.warning(f"Skip deleted or geo-restricted video: {entry['title']}")
+                return res
+            raise
+        except Exception as e:  # noqa: BLE001
+            logger.error(e)
+            return res
+        logger.warning(f"Found a new video: {entry['title']}")
+        res["need_download"] = True
+        return res
+
+
+async def main():
+    logger.info(f"Processing {args.name}")
+    # initialize bilibili
+    conf: dict = next(x for x in load_json(args.config) if x["name"] == args.name)
+    bilibili = Bilibili(args.name, conf, Path(args.metadata_dir) / f"{args.name}.json")
+    # process feed
+    processed_vids = {x["vid"] for x in bilibili.database}
+    remote = feedparser.parse(f"{os.getenv('RSSHUB_URL', 'https://rsshub.app')}/bilibili/user/video/{conf['uid']}")
+    for entry in remote["entries"][:5][::-1]:  # 5 videos from oldest to latest
+        vid = Path(entry["link"]).stem # type: ignore
+        if vid in processed_vids:
+            logger.debug(f"Skip processed: {entry['title']}")
+            continue
+        logger.info(f"New video found: [{entry['link']}] {entry['title']}")
+        res = await bilibili.process_single_entry(entry, use_cookie=False)
+        # Update
+        bilibili.update_database(res["entry_info"])
+        if not res["download_info"]:
+            continue
+
+        # get cover url
+        if re.search(r'img src="(.*)"', entry["summary"]):  # type: ignore # noqa: SIM108
+            cover = re.search(r'img src="(.*)"', entry["summary"]).group(1)  # type: ignore
+        else:
+            cover = conf.get("cover", "")
+
+        # audio
+        if not conf.get("skip_audio", False):
+            bilibili.upload_files("audio", res["download_info"]["audio_info"], vid)
+            audio_items = bilibili.get_pod_items(
+                pod_type="audio",
+                info_list=res["download_info"]["audio_info"],
+                vid=vid,
+                entry=entry,
+                cover=cover,
+            )
+            bilibili.update_pod_rss("audio", audio_items, feed=remote["feed"]) # type: ignore
+
+        # video
+        if not conf.get("skip_video", False):
+            bilibili.upload_files("video", res["download_info"]["video_info"], vid)
+            video_items = bilibili.get_pod_items(
+                pod_type="video",
+                info_list=res["download_info"]["video_info"],
+                vid=vid,
+                entry=entry,
+                cover=cover,
+            )
+            bilibili.update_pod_rss("video", video_items, feed=remote["feed"]) # type: ignore
+        # Cleanup
+        prefix = entry["title"][:60]
+        delete_files(Path(".").glob(f"{prefix}*"))
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Sync Bilibili to Telegram")
+    parser.add_argument("--log-level", type=str, default="INFO", required=False, help="Log level")
+    parser.add_argument("--config", type=str, default="config/bilibili.json", required=False, help="Path to configuration json file.")
+    parser.add_argument("--metadata-dir", type=str, default="metadata", required=False, help="Path to metadata directory.")
+    parser.add_argument("--name", type=str, required=True, help="Feed name.")
+    args = parser.parse_args()
+
+    # loguru settings
+    logger.remove()  # Remove default handler.
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        level=args.log_level,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>| <level>{level: <7}</level> | <cyan>{name: <10}</cyan>:<cyan>{function: ^30}</cyan>:<cyan>{line: >4}</cyan> - <level>{message}</level>",
+    )
+    asyncio.run(main())
---
+++ podsync/refresh-opml.py
@@ -0,0 +1,87 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+import xmltodict
+from github import gh
+from loguru import logger
+from utils import load_xml
+from videogram.utils import load_json
+from videogram.ytdlp import ytdlp_extract_info
+
+
+def get_youtube_description(yt_channel: str) -> str:
+    info: list[dict] = ytdlp_extract_info(f"https://www.youtube.com/channel/{yt_channel}", playlist=False, process=False)
+    return info[0]["description"] if info[0]["description"].strip() else info[0]["uploader"]
+
+
+def get_new_feeds(pod_type: str) -> tuple[bool, dict]:
+    assert pod_type in {"audio", "video"}
+    opml_path = f"{pod_type}/podsync.opml"
+    opml_data = load_xml(opml_path, template="opml")
+    opml_feeds = opml_data["opml"]["body"]["outline"]
+    if isinstance(opml_feeds, dict):
+        opml_feeds = [opml_feeds]
+    exist_feeds = [Path(feed["@xmlUrl"]).stem for feed in opml_feeds]
+    logger.debug(f"Found {len(exist_feeds)} existing feeds of {pod_type}")
+    conf_feed_names = []
+    for conf_file in Path(args.config_path).glob("*.json"):
+        conf_data = load_json(conf_file)
+        conf_feed_names.extend(x["name"] for x in conf_data if not x.get(f"skip_{pod_type}"))
+        for conf in conf_data:
+            if conf["name"] in exist_feeds:
+                continue
+
+            if conf.get("yt_channel") and not conf.get(f"skip_{pod_type}"):  # noqa: SIM108
+                description = get_youtube_description(conf["yt_channel"])
+            else:
+                description = conf["title"]
+
+            opml_feeds.append(
+                {
+                    "@text": description,
+                    "@type": "rss",
+                    "@xmlUrl": f"https://github.com/{os.environ['REPOSITORY']}/releases/download/{pod_type}/{conf['name']}.xml",
+                    "@title": conf["title"],
+                }
+            )
+    logger.debug(f"Found {len(conf_feed_names)} config feeds of {pod_type}")
+    has_update = set(exist_feeds) != set(conf_feed_names)
+    # remove feeds not in configuration any more.
+    filtered_feeds = [feed for feed in opml_feeds if Path(feed["@xmlUrl"]).stem in conf_feed_names]
+    opml_data["opml"]["body"]["outline"] = sorted(filtered_feeds, key=lambda x: x["@xmlUrl"])
+    return has_update, opml_data
+
+
+def main():
+    for pod_type in ["audio", "video"]:
+        has_update, new_opml = get_new_feeds(pod_type)
+        if has_update:
+            logger.info(f"Updating {pod_type} feeds")
+            opml_path = f"{pod_type}/podsync.opml"
+            with open(opml_path, "w") as f:
+                f.write(xmltodict.unparse(new_opml, pretty=True, full_document=True))
+            gh.upload_release(opml_path, pod_type)
+
+
+if __name__ == "__main__":
+    # parse arguments
+    parser = argparse.ArgumentParser(description="Sync YouTube to Telegram")
+    parser.add_argument("--log-level", type=str, default="INFO", required=False, help="Log level")
+    parser.add_argument("--config-path", type=str, default="config", required=False, help="Directory path of config json files.")
+    args = parser.parse_args()
+
+    # loguru settings
+    logger.remove()  # Remove default handler.
+    logger.add(
+        sys.stderr,
+        colorize=True,
+        level=args.log_level,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green>| <level>{level: <7}</level> | <cyan>{name: <10}</cyan>:<cyan>{function: ^30}</cyan>:<cyan>{line: >4}</cyan> - <level>{message}</level>",
+    )
+    main()
